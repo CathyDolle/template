6c72becdeefcb70dd0e4de1272cf47d76c63c806
Cathy <45395549+CathyDolle@users.noreply.github.com>
2022-09-24 11:30:40 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './styles/App.scss';
+import './styles/Library.scss';
+import { Routes, Route } from "react-router-dom";
+
+import Page1 from './pages/Page1';
+import Home from './Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" exact element={<Home />}/>
+      <Route path="/page1" element={<Page1 />}/>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import './styles/App.scss';
+
+function Home() {
+  return (
+    <div>
+        <h1>Hello beautiful people</h1>
+    </div>
+  );
+}
+
+export default Home;
